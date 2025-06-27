8495bc2b7b308f1f8ed6eb4294381ead235c6761
Gus <gus.dom.gtz@gmail.com>
2025-06-26 21:11:03 -0600

    Clase Pelicula con notacion JPA

--- tiendaPeliculas/src/main/java/com/metaphorce/tiendaPeliculas/entidades/Pelicula.java
+++ tiendaPeliculas/src/main/java/com/metaphorce/tiendaPeliculas/entidades/Pelicula.java
@@ -1,35 +1,42 @@
 package com.metaphorce.tiendaPeliculas.entidades;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Peliculas")
 public class Pelicula {
     // ATRIBUTOS
-    private Integer id;
-    private String nombre;
+    @Id // Definimos lo siguiente como id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Obtenemos la generación automática del id de MySQL
+    private Integer id_pelicula;
+    private String nombre_pelicula;
     private boolean disponible;
 
     // CONSTRUCTORES
     public Pelicula() {
     }
-    public Pelicula(Integer id, String nombre, boolean disponible) {
-        this.id = id;
-        this.nombre = nombre;
+    public Pelicula(Integer id_pelicula, String nombre_pelicula, boolean disponible) {
+        this.id_pelicula = id_pelicula;
+        this.nombre_pelicula = nombre_pelicula;
         this.disponible = disponible;
     }
-    
+
     // GET-SET
-    public Integer getId() {
-        return id;
+    public Integer getId_pelicula() {
+        return id_pelicula;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setId_pelicula(Integer id) {
+        this.id_pelicula = id;
     }
 
-    public String getNombre() {
-        return nombre;
+    public String getNombre_pelicula() {
+        return nombre_pelicula;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setNombre_pelicula(String nombre) {
+        this.nombre_pelicula = nombre;
     }
 
     public boolean isDisponible() {
